d32f7b73247abd126a1908bd220e3d81f9f6fefd
Kaburu12 <michael.kaburur@student.moringaschool.com>
2022-06-14 20:04:57 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
-const init = () => {
-  
-}
 
-document.addEventListener('DOMContentLoaded', init);
+const init = () => {
+	const inputForm = document.querySelector('form')
+  
+	inputForm.addEventListener('submit', (event) => {
+	  event.preventDefault();
+	  const input = document.querySelector('input#searchByID');
+  
+	  fetch(`http://localhost:3000/movies/${input.value}`)
+	  .then(response => response.json())
+		  .then(data => {
+			console.log(data);
+  //Let's replace Title and Summary with data we retrieved from our server
+			  
+		const title = document.querySelector('section#movieDetails h4');
+		const summary = document.querySelector('section#movieDetails p');
+  //Next, we want to change the contents of our title and summary elements
+			  
+		title.innerText = data.title;
+		summary.innerText = data.summary;
+	  });
+	});
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
